6cbf7a87b5970c19e5ae0587349acde5000587e5
Zheng <limzhengwei_99@hotmail.com>
2021-06-07 15:58:09 +0800

    Add autocomplete

--- lib/events/dto/eventCreate.vc.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { VerifiableCredentialDTO } from '../../general';
-import { CORE_EventCreateCredentialSubjectDTO } from './eventCreate.credentialSubject.dto';
-export declare class EventCreateVCDTO extends VerifiableCredentialDTO {
-    '@context': string[];
-    type: string[];
-    credentialSubject: CORE_EventCreateCredentialSubjectDTO;
-}
