1f4a32d664eeb8e9b048cf1bed20b9e7b5b5b8a7
noredeen <noredeenalzubi@gmail.com>
2021-07-06 14:24:05 -0400

    rebuild

--- lib/events/dto/addEntryDetails/event.credentialSubject.dto.d.ts
+++ lib/events/dto/addEntryDetails/event.credentialSubject.dto.d.ts
@@ -4,21 +4,19 @@ import { ParcelDeliveryDTO } from 'mavennet-dto';
 export declare class AGENT_AddEntryDetailsCredentialSubjectDTO {
     eventId: string;
     productId: string;
-    eventTime: Date;
     type: string[];
+    transactionNumber: string;
+}
+export declare class CORE_AddEntryDetailsCredentialSubjectDTO extends AGENT_AddEntryDetailsCredentialSubjectDTO {
+    '@context': string[];
+    eventTime: Date;
     portOfEntry: PlaceDTO;
     carrier: OrganizationDTO;
     recipient: OrganizationDTO;
     portOfDestination: PlaceDTO;
     shipment: ParcelDeliveryDTO;
-    transactionNumber: string;
     expectedDeliveryDate: Date;
     valuePerItem: string;
     totalOrderValue: string;
     product: ProductDTO;
-}
-export declare class CORE_AddEntryDetailsCredentialSubjectDTO extends AGENT_AddEntryDetailsCredentialSubjectDTO {
-    '@context': string[];
-    eventId: string;
-    productId: string;
 }
--- lib/events/dto/addEntryDetails/event.vc.dto.d.ts
+++ lib/events/dto/addEntryDetails/event.vc.dto.d.ts
@@ -1,6 +1,11 @@
 import { VerifiableCredentialDTO, JSON_TYPE } from '../../../general';
-import { CORE_AddEntryDetailsCredentialSubjectDTO } from './event.credentialSubject.dto';
-export declare class AddEntryDetailsVCDTO extends VerifiableCredentialDTO {
+import { AGENT_AddEntryDetailsCredentialSubjectDTO, CORE_AddEntryDetailsCredentialSubjectDTO } from './event.credentialSubject.dto';
+export declare class BASE_AddEntryDetailsVCDTO extends VerifiableCredentialDTO {
+    '@context': string[];
+    type: JSON_TYPE[];
+    credentialSubject: AGENT_AddEntryDetailsCredentialSubjectDTO;
+}
+export declare class CORE_AddEntryDetailsVCDTO extends VerifiableCredentialDTO {
     '@context': string[];
     type: JSON_TYPE[];
     credentialSubject: CORE_AddEntryDetailsCredentialSubjectDTO;
