09854cc083d58fa2045b290258bc145dd06f38cb
vivienfan <vivien.fan.1224@gmail.com>
2021-07-06 10:50:02 -0400

    rebuild

--- lib/products/constants/productName.d.ts
+++ lib/products/constants/productName.d.ts
@@ -6,7 +6,6 @@ export declare enum PRODUCT_NAME {
     CARBON_ALLOY_SEMI = "Carbon and alloy semi-finished products",
     CARBON_ALLOY_FLAT = "Carbon and alloy flat product",
     CARBON_ALLOY_PIPE_TUBE = "Carbon and alloy pipe and tube products",
-    CARBON_AND_ALLOY_LONG_PRODUCT = "Carbon and alloy long product",
-    STAINLESS_STEEL_PRODUCTS = "Stainless steel products",
-    CARBON_AND_ALLOY_PIPE_AND_TUBE_PRODUCTS = "Carbon and alloy pipe and tube products"
+    CARBON_ALLOY_LONG = "Carbon and alloy long product",
+    STAINLESS_STEEL = "Stainless steel products"
 }
