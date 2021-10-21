3a4e791fb00d50eaf6b8ce329a41ba6afe592978
anserghazi <anserghazi@gmail.com>
2021-10-20 23:46:33 -0400

    fix(ProductVCDTO): added ApiProperty decorator to ProductVCDTO

--- src/products/dto/product.vc.dto.ts
+++ src/products/dto/product.vc.dto.ts
@@ -9,6 +9,7 @@ import {
   ValidateNested,
   Matches
 } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import { JSON_TYPE } from '../../general/constants'
 import { VerifiableCredentialDTO } from '../../general/dto/verifiableCredential.dto'
@@ -17,33 +18,40 @@ import { ProductCredentialSubjectDTO } from './productCredentialSubject.dto'
 class ProductVCDTOBase {
   @IsArray()
   @ArrayMinSize(1)
+  @ApiProperty()
   '@context': string[]
 
   @IsNotEmpty()
   @IsUrl({ require_tld: false })
+  @ApiProperty()
   id: string
 
   @IsArray()
   @ArrayMinSize(1)
+  @ApiProperty({ enum: JSON_TYPE, isArray: true })
   type: JSON_TYPE[]
 
   @IsNotEmpty()
   @IsString()
   @Matches(/^did:/)
+  @ApiProperty()
   issuer: string
 
   @IsNotEmpty()
   @IsDateString()
+  @ApiProperty()
   issuanceDate: string
 
   @IsNotEmpty()
   @ValidateNested()
   @Type(() => ProductCredentialSubjectDTO)
+  @ApiProperty()
   credentialSubject: ProductCredentialSubjectDTO
 
   @IsNotEmptyObject()
   @ValidateNested()
   @Type(() => VerifiableCredentialDTO)
+  @ApiProperty()
   proof: VerifiableCredentialDTO
 }
 
