4f8f4b77db217a6159da06efc73e7bbf47f00532
Vraj Patel <39704588+ChocoMufn@users.noreply.github.com>
2022-04-04 09:10:37 -0400

    credential subject added

--- src/events/dto/transport/QPinbond/create.dto.ts
+++ src/events/dto/transport/QPinbond/create.dto.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty, IsNotEmptyObject, ValidateNested } from 'class-validator'
+import { IsNotEmpty, IsNotEmptyObject, IsOptional, ValidateNested } from 'class-validator'
 import { Type } from 'class-transformer'
 import { QPInBondEventVCDTO } from './event.vc.dto'
+import { QPInbondCredentialSubjectDTO } from '.'
 
 export class CreateQPInbondDTO {
   @IsNotEmpty()
@@ -13,4 +14,9 @@ export class CreateQPInbondDTO {
   @ValidateNested()
   @Type(() => QPInBondEventVCDTO)
   eventVC: QPInBondEventVCDTO
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => QPInbondCredentialSubjectDTO)
+  credentialSubject: QPInbondCredentialSubjectDTO
 }
