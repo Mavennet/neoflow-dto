9b7c55d70c025789ad1aa4d4a9ab1120a671b4e9
Yousif Al-Ogaidi <haqi.yousef@gmail.com>
2022-07-14 13:27:44 +0300

    removed unused import

--- src/products/dto/sharing/shareProduct.dto.ts
+++ src/products/dto/sharing/shareProduct.dto.ts
@@ -1,6 +1,5 @@
 import { IsNotEmpty, IsUUID, IsArray, ArrayMinSize, IsString } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
-import { Type } from 'class-transformer'
 
 export class ShareProductDTO {
   @ApiProperty()
