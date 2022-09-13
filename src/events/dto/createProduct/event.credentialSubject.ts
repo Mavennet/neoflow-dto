accca1546204e6a34efdc405c87f4c814abed84f
vivienfan <vivien.fan.1224@gmail.com>
2022-09-13 16:12:40 -0400

    allow locahost

--- src/events/dto/createProduct/event.credentialSubject.ts
+++ src/events/dto/createProduct/event.credentialSubject.ts
@@ -38,6 +38,6 @@ export class CORE_CreationEventCredentialSubjectDTO extends AGENT_CreationEventC
   @ApiProperty()
   @IsArray()
   @ArrayMinSize(1)
-  @IsUrl({ protocols: ['http', 'https'] }, { each: true })
+  @IsUrl({ protocols: ['http', 'https'], require_tld: false }, { each: true })
   products: string[]
 }
--- src/events/dto/transfer/event.credentialSubject.dto.ts
+++ src/events/dto/transfer/event.credentialSubject.dto.ts
@@ -24,7 +24,7 @@ export class AGENT_TransferEventCredentialSubjectDTO {
 
   @IsArray()
   @ArrayMinSize(1)
-  @IsUrl({ protocols: ['http', 'https'] }, { each: true })
+  @IsUrl({ protocols: ['http', 'https'], require_tld: false }, { each: true })
   products: string[]
 
   @IsArray()
