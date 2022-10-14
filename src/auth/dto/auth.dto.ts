bf7b0280ab8310ff9f40cd64ef67dda6414a7cd9
Yousif Al-Ogaidi <haqi.yousef@gmail.com>
2022-10-14 14:16:40 -0400

    ass string validation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,15 +1,19 @@
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, IsString } from 'class-validator'
 
 export class AuthDTO {
   @IsNotEmpty()
+  @IsString()
   audience: string
-
+  
   @IsNotEmpty()
+  @IsString()
   client_id: string
-
+  
   @IsNotEmpty()
+  @IsString()
   client_secret: string
-
+  
   @IsNotEmpty()
+  @IsString()
   grant_type: string
 }
