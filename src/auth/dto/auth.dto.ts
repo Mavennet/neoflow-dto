ed467eba5a0248cc913b879f4bcdee68a6c23641
Yousif Al-Ogaidi <haqi.yousef@gmail.com>
2022-10-17 14:51:15 -0400

    build + formatting

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -4,15 +4,15 @@ export class AuthDTO {
   @IsNotEmpty()
   @IsString()
   audience: string
-  
+
   @IsNotEmpty()
   @IsString()
   client_id: string
-  
+
   @IsNotEmpty()
   @IsString()
   client_secret: string
-  
+
   @IsNotEmpty()
   @IsString()
   grant_type: string
