869537c7efb6e2e36bc6a46ec5edd3665b07ce00
Khalid Gobin <44628334+kgobin2000@users.noreply.github.com>
2023-07-13 19:51:59 -0400

    change sumOfScheduledDeliveries to measurement

--- lib/advance-manifest/advanceManifest.credentialSubject.dto.d.ts
+++ lib/advance-manifest/advanceManifest.credentialSubject.dto.d.ts
@@ -1,11 +1,11 @@
-import { JSON_TYPE, JSON_TYPE_NF } from '../general';
+import { JSON_TYPE, JSON_TYPE_NF, MeasurementDTO } from '../general';
 export declare class AdvanceManifestCredentialSubjectDTO {
     type: Array<JSON_TYPE | JSON_TYPE_NF>;
     date: string;
     scheduledDeliveries?: Array<{
         portCode: string;
         deliveryLocation: string;
-        sumOfScheduledDeliveries: string;
+        sumOfScheduledDeliveries: MeasurementDTO[];
         numberOfBatches: string;
     }>;
 }
