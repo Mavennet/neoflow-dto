00708a94ba0f17e9af9fa70843cfdea1cbb5b184
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2023-08-10 15:38:32 -0400

    missing build files

--- lib/products/dto/product.vc.dto.d.ts
+++ lib/products/dto/product.vc.dto.d.ts
@@ -1,5 +1,5 @@
-import { ProductCredentialSubjectDTO } from './productCredentialSubject.dto';
 import { VerifiableCredentialDTO } from '@mavennet/traceability-dto';
+import { ProductCredentialSubjectDTO } from './productCredentialSubject.dto';
 export declare class ProductVCDTO extends VerifiableCredentialDTO implements Omit<VerifiableCredentialDTO, 'credentialSubject'> {
     credentialSubject: ProductCredentialSubjectDTO;
 }
