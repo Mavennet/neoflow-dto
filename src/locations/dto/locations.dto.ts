03533d0363e4a4627b7fd107d8eb78edefc4adca
vivienfan <vivien.fan.1224@gmail.com>
2023-09-07 13:11:48 -0400

    add locations dto

---
+++ src/locations/dto/locations.dto.ts
@@ -0,0 +1,77 @@
+import { IsArray, IsString, IsObject, ValidateNested } from 'class-validator'
+import { Type } from 'class-transformer'
+
+class CoordinatesDto {
+	@IsString()
+	lat: string
+
+	@IsString()
+	lng: string
+}
+
+class PostalAddressDto {
+	@IsString()
+	streetAddress: string
+
+	@IsString()
+  addressLocality: string
+  
+	@IsString()
+  addressRegion: string
+  
+	@IsString()
+  postalCode: string
+  
+	@IsString()
+  addressCountry: string
+}
+
+class DetailsDto {
+	@IsString()
+	address: string
+
+	@ValidateNested()
+  @Type(() => CoordinatesDto)
+	coordinates: CoordinatesDto
+
+	@ValidateNested()
+  @Type(() => PostalAddressDto)
+	postalAddress: PostalAddressDto
+}
+
+export class LocationDto {
+	@IsString()
+  value: string
+
+	@IsString()
+  label: string
+  
+	@IsString({ each: true })
+  alias: string[]
+
+	@IsString()
+  firmsCode: string
+  
+	@IsString()
+  portCode: string
+  
+  @IsObject()
+  details: DetailsDto
+}
+
+export class LocationsDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => LocationDto)
+  deliveryFacilities: LocationDto[]
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => LocationDto)
+  receiptLocations: LocationDto[]
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => LocationDto)
+  ports: LocationDto[]
+}
