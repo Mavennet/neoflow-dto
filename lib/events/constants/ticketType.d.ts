833ca24956931da2261ac2f30198f708f329b893
Guilherme Guimaraes <fg.guilherme@gmail.com>
2024-04-16 12:08:22 -0400

    add ticket type to event DTO as optional

---
+++ lib/events/constants/ticketType.d.ts
@@ -0,0 +1,5 @@
+export declare enum TICKET_TYPE {
+    METER_CUSTODY = "METER CUSTODY",
+    ADJUSTMENT = "ADJUSTMENT",
+    SPLIT = "SPLIT"
+}
--- lib/events/dto/deliveryTicket/event.vc.dto.d.ts
+++ lib/events/dto/deliveryTicket/event.vc.dto.d.ts
@@ -1,6 +1,7 @@
 import { ProofDTO } from '../../../general/dto/proof.dto';
 import { DeliveryTicketCredentialSubjectDTO } from './event.credentialSubject.dto';
 import { JSON_TYPE_NF } from '../../../general';
+import { TICKET_TYPE } from '../../constants';
 export declare class DeliveryTicketVCDTO {
     '@context': string[];
     id: string;
@@ -9,4 +10,5 @@ export declare class DeliveryTicketVCDTO {
     issuanceDate?: string | Date;
     credentialSubject: DeliveryTicketCredentialSubjectDTO;
     proof: ProofDTO;
+    ticketType?: TICKET_TYPE;
 }
