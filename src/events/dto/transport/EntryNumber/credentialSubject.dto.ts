72ae342e27751f05ad12d28bb6a46fcdb6bb15d7
rik <vslimrik@gmail.com>
2024-11-25 12:39:13 -0500

    update entry number credential subject

--- src/events/dto/transport/EntryNumber/credentialSubject.dto.ts
+++ src/events/dto/transport/EntryNumber/credentialSubject.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { ArrayNotEmpty, IsArray, IsEnum, IsNotEmpty, IsString } from 'class-validator'
+import { ArrayNotEmpty, IsArray, IsBoolean, IsEnum, IsNotEmpty, IsString } from 'class-validator'
 import { JSON_TYPE } from '../../../../general/constants'
 
 export class EntryNumberCredentialSubjectDTO {
@@ -13,4 +13,14 @@ export class EntryNumberCredentialSubjectDTO {
   @IsNotEmpty()
   @IsString()
   entryNumber: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsBoolean()
+  usmca: boolean
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsBoolean()
+  selfProduced: boolean
 }
