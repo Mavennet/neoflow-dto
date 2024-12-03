18fe8732313474d81b2e14f63060449114863729
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2024-12-03 12:37:34 -0500

    add monthly statements, update lint

--- lib/events/dto/transport/event.vc.dto.d.ts
+++ lib/events/dto/transport/event.vc.dto.d.ts
@@ -1,5 +1,5 @@
-import { AGENT_TransportationEventCredentialSubjectDTO, CORE_TransportationEventCredentialSubjectDTO } from './event.credentialSubject.dto';
 import { CredentialDTO } from '../../../credentials';
+import { AGENT_TransportationEventCredentialSubjectDTO, CORE_TransportationEventCredentialSubjectDTO } from './event.credentialSubject.dto';
 export declare class CORE_TransportationEventDetailsDTO extends CredentialDTO<CORE_TransportationEventCredentialSubjectDTO> {
     credentialSubject: CORE_TransportationEventCredentialSubjectDTO;
 }
--- src/events/dto/transport/event.vc.dto.ts
+++ src/events/dto/transport/event.vc.dto.ts
@@ -1,20 +1,10 @@
 import { Type } from 'class-transformer'
-import {
-  ArrayMinSize,
-  IsArray,
-  IsDateString,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  Matches,
-  ValidateNested
-} from 'class-validator'
-import { ProofDTO } from '../../../general/dto/proof.dto'
+import { IsNotEmpty, ValidateNested } from 'class-validator'
+import { CredentialDTO } from '../../../credentials'
 import {
   AGENT_TransportationEventCredentialSubjectDTO,
   CORE_TransportationEventCredentialSubjectDTO
 } from './event.credentialSubject.dto'
-import { CredentialDTO } from '../../../credentials'
 
 export class CORE_TransportationEventDetailsDTO extends CredentialDTO<CORE_TransportationEventCredentialSubjectDTO> {
   @IsNotEmpty()
