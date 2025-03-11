1fdf6473229c094976c8740f0a3bcd4be96dc8de
kaijie0102 <leowkaijie@gmail.com>
2025-03-11 11:55:57 -0400

    Add DeliveryTicketResponseDto for core

--- lib/events/dto/deliveryTicket/deliveryTicket.dto.d.ts
+++ lib/events/dto/deliveryTicket/deliveryTicket.dto.d.ts
@@ -1,15 +1,25 @@
-import { DeliveryTicketCredentialSubjectDTO } from './event.credentialSubject.dto';
-import { EnvelopedVerifiableCredential } from '../../../credentials';
+import { DeliveryTicketCredentialSubjectDTO } from './event.credentialSubject.dto'
+import { EnvelopedVerifiableCredential } from '../../../credentials'
 export declare class CORE_DeliveryTicketDTO {
-    productId: string;
-    gasShipmentId: string;
-    uploadFileKey?: string;
-    eventId: string;
-    eventVC: EnvelopedVerifiableCredential;
+  productId: string
+  gasShipmentId: string
+  uploadFileKey?: string
+  eventId: string
+  eventVC: EnvelopedVerifiableCredential
 }
 export declare class AGENT_DeliveryTicketDTO {
-    productId: string;
-    gasShipmentId: string;
-    uploadFileKey?: string;
-    credentialSubject: DeliveryTicketCredentialSubjectDTO;
+  productId: string
+  gasShipmentId: string
+  uploadFileKey?: string
+  credentialSubject: DeliveryTicketCredentialSubjectDTO
+}
+
+export declare enum STATUS {
+  SUCESS = 'SUCESS',
+  DUPLICATE = 'DUPLICATE'
+}
+
+export declare class CORE_DeliveryTicketResponseDto {
+  status: STATUS
+  eventVC?: EnvelopedVerifiableCredential
 }
