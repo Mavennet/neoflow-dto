e92c2a148f839276dc906eef1acc22090729c986
kaijie0102 <leowkaijie@gmail.com>
2025-03-14 16:42:11 -0400

    Add enum and classes to track delivery ticket response

--- lib/events/dto/deliveryTicket/deliveryTicket.dto.d.ts
+++ lib/events/dto/deliveryTicket/deliveryTicket.dto.d.ts
@@ -1,25 +1,23 @@
-import { DeliveryTicketCredentialSubjectDTO } from './event.credentialSubject.dto'
-import { EnvelopedVerifiableCredential } from '../../../credentials'
+import { DeliveryTicketCredentialSubjectDTO } from './event.credentialSubject.dto';
+import { EnvelopedVerifiableCredential } from '../../../credentials';
 export declare class CORE_DeliveryTicketDTO {
-  productId: string
-  gasShipmentId: string
-  uploadFileKey?: string
-  eventId: string
-  eventVC: EnvelopedVerifiableCredential
+    productId: string;
+    gasShipmentId: string;
+    uploadFileKey?: string;
+    eventId: string;
+    eventVC: EnvelopedVerifiableCredential;
 }
 export declare class AGENT_DeliveryTicketDTO {
-  productId: string
-  gasShipmentId: string
-  uploadFileKey?: string
-  credentialSubject: DeliveryTicketCredentialSubjectDTO
+    productId: string;
+    gasShipmentId: string;
+    uploadFileKey?: string;
+    credentialSubject: DeliveryTicketCredentialSubjectDTO;
 }
-
-export enum DELIVERY_TICKET_STATUS {
-  SUCCESS = 'SUCCESS',
-  DUPLICATE = 'DUPLICATE'
+export declare enum DELIVERY_TICKET_STATUS {
+    SUCCESS = "SUCCESS",
+    DUPLICATE = "DUPLICATE"
 }
-
 export declare class CORE_DeliveryTicketResponseDto {
-  deliveryTicketStatus: DELIVERY_TICKET_STATUS
-  eventVC?: EnvelopedVerifiableCredential
+    deliveryTicketStatus: DELIVERY_TICKET_STATUS;
+    eventVC?: EnvelopedVerifiableCredential;
 }
