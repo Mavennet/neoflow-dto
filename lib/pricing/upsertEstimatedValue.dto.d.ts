bb7aae7ad1eac3ff8da7b4d085b9b80e997d5e3c
rik <vslimrik@gmail.com>
2025-05-23 12:45:22 -0400

    add id to upsert estimated pricing dto

--- lib/pricing/upsertEstimatedValue.dto.d.ts
+++ lib/pricing/upsertEstimatedValue.dto.d.ts
@@ -1,5 +1,6 @@
 import { PricingStreams } from './pricingStreams.dto';
 export declare class UpsertEstimatedValueDTO {
+    id?: number;
     startDate: Date;
     endDate: Date;
     streams: PricingStreams[];
--- src/pricing/upsertEstimatedValue.dto.ts
+++ src/pricing/upsertEstimatedValue.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsArray, IsDate, IsNotEmpty } from 'class-validator'
+import { IsArray, IsDate, IsNotEmpty, IsNumber, IsOptional } from 'class-validator'
 import { PricingStreams } from './pricingStreams.dto'
 import { Type } from 'class-transformer'
 
 export class UpsertEstimatedValueDTO {
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  id?: number
+
   @ApiProperty()
   @IsNotEmpty()
   @IsDate()
