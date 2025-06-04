0e2dfe86b0c5376ddaf66a548f24e01fc4b40463
Khalid Gobin <khalidgobin@hotmail.com>
2025-06-04 10:03:52 -0400

    decommission qldb

--- lib/events/dto/inspect/inspect.dto.d.ts
+++ lib/events/dto/inspect/inspect.dto.d.ts
@@ -1,9 +1,9 @@
 import { InspectionEventCredentialSubjectDTO } from './event.credentialSubject.dto';
-import { InspectionEventDetailsDTO } from './event.vc.dto';
+import { EnvelopedVerifiableCredential } from '../../../credentials';
 export declare class CORE_InspectProductDTO {
     productId: string;
     eventId: string;
-    eventVC: InspectionEventDetailsDTO;
+    eventVC: EnvelopedVerifiableCredential;
 }
 export declare class AGENT_InspectProductDTO {
     productId: string;
