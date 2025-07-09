e0407207d5dbc59d82d6b45d947b49f3b975b4c0
Leow Kai Jie <leowkaijie@gmail.com>
2025-07-09 13:59:21 -0400

    add constants SCAC, HTS, export Province Codes

---
+++ lib/credentials/constants/verifiableCredentialFields.d.ts
@@ -0,0 +1,24 @@
+export declare const standardCarrierAlphaCode: {
+    readonly ENBRIDGE: "1";
+    readonly SOUTHBOW: "2";
+    readonly KEYSTONE: "3";
+};
+export declare const harmonizedTariffSchedule: {
+    readonly API_LOW: "2709.00.10";
+    readonly API_HIGH: "2709.00.20";
+};
+export declare const exportProvinceCode: {
+    readonly ALBERTA: "XA";
+    readonly BRITISH_COLUMBIA: "XC";
+    readonly MANITOBA: "XM";
+    readonly NEW_BRUNSWICK: "XB";
+    readonly NEWFOUNDLAND_LABRADOR: "XW";
+    readonly NORTHWEST_TERRITORIES: "XT";
+    readonly NOVA_SCOTIA: "XN";
+    readonly NUNAVUT: "XV";
+    readonly ONTARIO: "XO";
+    readonly PRINCE_EDWARD_ISLAND: "XP";
+    readonly QUEBEC: "XQ";
+    readonly SASKATCHEWAN: "XS";
+    readonly YUKON_TERRITORY: "XY";
+};
---
+++ src/credentials/constants/verifiableCredentialFields.ts
@@ -0,0 +1,26 @@
+export const standardCarrierAlphaCode = {
+  ENBRIDGE: '1',
+  SOUTHBOW: '2',
+  KEYSTONE: '3'
+} as const
+
+export const harmonizedTariffSchedule = {
+  API_LOW: '2709.00.10',
+  API_HIGH: '2709.00.20'
+} as const
+
+export const exportProvinceCode = {
+  ALBERTA: 'XA',
+  BRITISH_COLUMBIA: 'XC',
+  MANITOBA: 'XM',
+  NEW_BRUNSWICK: 'XB',
+  NEWFOUNDLAND_LABRADOR: 'XW',
+  NORTHWEST_TERRITORIES: 'XT',
+  NOVA_SCOTIA: 'XN',
+  NUNAVUT: 'XV',
+  ONTARIO: 'XO',
+  PRINCE_EDWARD_ISLAND: 'XP',
+  QUEBEC: 'XQ',
+  SASKATCHEWAN: 'XS',
+  YUKON_TERRITORY: 'XY'
+} as const
