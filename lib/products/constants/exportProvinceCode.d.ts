4009ac35318c1440dd74d3d9701221ed87361879
Leow Kai Jie <leowkaijie@gmail.com>
2025-07-11 13:50:09 -0400

    feat: modify htsCode, add SCAC and exportProvinceCode

---
+++ lib/products/constants/exportProvinceCode.d.ts
@@ -0,0 +1,15 @@
+export declare const ExportProvinceCode: {
+    readonly ALBERTA: "XA";
+    readonly BRITISH_COLUMBIA: "XC";
+    readonly MANITOBA: "XM";
+    readonly NEW_BRUNSWICK: "XB";
+    readonly NEWFOUNDLAND_LABRADOR: "XW";
+    readonly NORTHWEST_TERRITORIES: "XT";
+    readonly NOVA_SCOTIA: "XN";
+    readonly NUNAVUT: "XV";
+    readonly ONTARIO: "XO";
+    readonly PRINCE_EDWARD_ISLAND: "XP";
+    readonly QUEBEC: "XQ";
+    readonly SASKATCHEWAN: "XS";
+    readonly YUKON_TERRITORY: "XY";
+};
---
+++ lib/products/constants/standardCarrierAlphaCode.d.ts
@@ -0,0 +1,6 @@
+export declare const StandardCarrierAlphaCode: {
+    readonly ENBRIDGE: "SCAC-1";
+    readonly EXPRESS: "SCAC-2";
+    readonly KEYSTONE: "SCAC-3";
+    readonly TMPL: "SCAC-4";
+};
