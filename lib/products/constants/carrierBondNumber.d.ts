9bf2dc2b747313a3c0d51534dae2ec429eb2aa7a
Leow Kai Jie <leowkaijie@gmail.com>
2025-07-11 14:36:04 -0400

    feat: add carrier bond ID

---
+++ lib/products/constants/carrierBondNumber.d.ts
@@ -0,0 +1,6 @@
+export declare const CarrierBondNumber: {
+    readonly ENBRIDGE: "CarrierBondID-1";
+    readonly EXPRESS: "CarrierBondID-2";
+    readonly KEYSTONE: "CarrierBondID-3";
+    readonly TMPL: "CarrierBondID-4";
+};
